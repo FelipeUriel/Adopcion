f1c23260a81af1eb2e2b37821b828f9c6303199a
oscarulises.yanez.contractor <oscarulises.yanez.contractor@bbva.com>
2023-08-05 16:57:45 -0600

    probando usuario commit:

---
+++ navBarSection.php
@@ -0,0 +1,25 @@
+
+<?php
+ echo '
+  <nav class="navbar navbar-expand-lg bg-none navbar-dark py-3">
+            <a href="index.html" class="navbar-brand px-lg-4 m-0">
+                <h1 class="m-0 display-4 text-uppercase text-white">Little Pet</h1>
+            </a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                <div class="navbar-nav ml-auto p-4">
+                    <a href="index.html" class="nav-item nav-link active">Inicio</a>
+                    <a href="SobreNosotros.html" class="nav-item nav-link">Sobre nosotros</a>
+                    <a href="Valores.html" class="nav-item nav-link">Valores</a>
+                    <a href="Paginas.html" class="nav-item nav-link">Categorias</a>
+                    <a href="Adoptar.html" class="nav-item nav-link">Adopción</a>
+                    <a href="DarenAdopcion.html"  class="nav-item nav-link">Dar en Adopcion</a>
+                    <div class="nav-item dropdown">
+                    <a href="Contacto.html" class="nav-item nav-link">Contacto</a>
+                </div>
+            </div>
+        </nav>
+        '
+        ?>
